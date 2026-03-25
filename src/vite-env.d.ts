5d8467bd8b7c9963892ceb79a8c2d00ce9867a8f
SpaceMan <naisonproject@gmail.com>
2026-03-25 19:21:18 +0000

    .

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string
+  readonly VITE_SUPABASE_PROJECT_ID: string
+  readonly GROQ_API_KEY: string
+  readonly TAVILY_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
